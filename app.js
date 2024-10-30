570aedd72e842a06cfad84b12a0691f2940120a8
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-30 15:26:51 +0100

    create route that returns a h1

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 /*Esercizio
 Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
-Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
 Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 Creiamo poi una rotta /posts che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array locale.
 La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
 Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
 Testare nel browser.*/
+
+//Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+    const markup = `
+    <h1>Benvenuto nel mio blog</h1>
+  `
+
+  res.send(markup);
+});
