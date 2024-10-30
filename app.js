887fde43f32b7744c5f83040fe90d6b27dd175ab
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-30 16:25:33 +0100

    create a server and create public img folder

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
 
 
-Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+
 Testare nel browser.*/
 
-//Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+//richiamo express nel file app.js
 const express = require('express');
 const app = express();
 
+const host = 'http://127.0.0.1'
+const port = 3000
+
+//accedo al server 
+app.listen(port, () => {
+  console.log(`Example app listening on ${host}:${port}`)
+})
+
+//Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+app.use(express.static('public'))
+
+//Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
 app.get('/', (req, res)=> {
 
   res.send('<h1>Benvenuto nel mio blog</h1>');
