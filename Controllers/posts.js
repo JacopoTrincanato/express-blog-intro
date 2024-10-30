c86347892e03e1c87fef300c74d4fd39f6963c21
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-30 17:13:43 +0100

    add images

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -3,37 +3,37 @@ const posts = [
     {
         titolo: "Introduzione a JavaScript",
         contenuto: "JavaScript è un linguaggio di programmazione utilizzato per rendere le pagine web interattive.",
-        immagine: "https://www.mistersito.com/wp-content/uploads/2022/05/javascript-a-cosa-serve.webp",
+        immagine: "../public/img/javascript-a-cosa-serve.webp",
         tags: ["JavaScript", "Programmazione", "Web Development"]
     },
     {
         titolo: "Le Basi di HTML",
         contenuto: "HTML è il linguaggio usato per strutturare il contenuto delle pagine web.",
-        immagine: "https://www.creativemotions.it/wp-content/uploads/2021/11/Che-cose-HTML-Le-basi-di-Hypertext-Markup-Language.jpg",
+        immagine: "../public/img/Che-cose-HTML-Le-basi-di-Hypertext-Markup-Language.webp",
         tags: ["HTML", "Web Development", "Struttura"]
     },
     {
         titolo: "CSS per lo Stile",
         contenuto: "CSS è utilizzato per aggiungere stile e layout ai documenti HTML.",
-        immagine: "https://www.creativemotions.it/wp-content/uploads/2021/11/Cos-e-il-css-768x403.jpg",
+        immagine: "../public/img/Cos-e-il-css-768x403.webp",
         tags: ["CSS", "Design", "Stile"]
     },
     {
         titolo: "Sviluppo Frontend vs Backend",
         contenuto: "Scopri la differenza tra lo sviluppo frontend e backend e come collaborano.",
-        immagine: "https://vitolavecchia.altervista.org/wp-content/uploads/2020/03/Caratteristiche-e-Differenza-tra-front-end-e-back-end-per-un-CMS.jpg",
+        immagine: "../public/img/Caratteristiche-e-Differenza-tra-front-end-e-back-end-per-un-CMS.jpg",
         tags: ["Frontend", "Backend", "Web Development"]
     },
     {
         titolo: "Introduzione a React",
         contenuto: "React è una libreria JavaScript popolare per la creazione di interfacce utente.",
-        immagine: "https://www.docety.com/images/videocorsi/179.jpg",
+        immagine: "../public/img/immagine_react.jpg",
         tags: ["React", "JavaScript", "Frontend"]
     },
     {
         titolo: "Costruire API con Node.js",
         contenuto: "Node.js permette di costruire API veloci e scalabili per applicazioni web.",
-        immagine: "https://i.ytimg.com/vi/-VOHGCdhHmY/maxresdefault.jpg",
+        immagine: "../public/img/node_express_img.jpg",
         tags: ["Node.js", "Backend", "API"]
     }
 ];
