db4020203bd9965177b659a6b9edee64e9e6dd40
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-30 15:56:53 +0100

    create index function in posts.js

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -37,3 +37,11 @@ const posts = [
         tags: ["Node.js", "Backend", "API"]
     }
 ];
+
+//creo una funzione index che restituisce un oggetto json con la lista dei post e il conteggio, partendo dall'array posts
+function index(req, res) {
+    res.json({
+      data: posts,
+      count: posts.length
+    })
+  }
